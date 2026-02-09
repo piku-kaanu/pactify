47317959be7c4172050bdbfc258744ac5e3e32d2
piku-kaanu <parthkansara11@gmail.com>
2026-02-09 18:29:50 +0530

    resolved version

--- pactify/cli/main.py
+++ pactify/cli/main.py
@@ -1,15 +1,5 @@
-import sys
-
-from pactify import __version__
+from pactify.__version__ import __version__
 
 
-def main() -> None:
-    """
-    pactify CLI entrypoint.
-    """
-    if "--version" in sys.argv:
-        print(__version__)
-        sys.exit(0)
-
-    print("pactify CLI is under active development.")
-    sys.exit(0)
+def main():
+    print(__version__)
